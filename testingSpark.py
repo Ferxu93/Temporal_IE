bfcefe8f2efb1f029a6df08037e84f3373e40307
Fer <fllopisber@student.ie.edu>
2020-03-22 21:44:08 +0100

    not necessary

--- testingSpark.py
+++ testingSpark.py
@@ -5,6 +5,15 @@ from pyspark.sql import SparkSession
 from pyspark import SparkConf
 import os
 import findspark
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+%matplotlib inline
+import mpld3
+import pyspark
+from pyspark.sql import SparkSession
+from pyspark.sql.types import *
+import pyspark.sql.functions as F
 
 spark_home = os.environ.get('SPARK_HOME', None)
 
